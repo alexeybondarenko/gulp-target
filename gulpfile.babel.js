6c6ce8561fb51c0e16c0498690dea63e4389fda1
Alexey Bondarenko <alexeybondarenko@me.com>
2016-03-16 11:22:51 +0200

    Add sample export task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,23 +8,51 @@ import sass from 'gulp-sass';
 import watch from 'gulp-watch';
 import {argv} from 'yargs';
 
-var isWatching = argv.watch || false;
-var target = new Target('targetA');
+import lazypipe from 'lazypipe';
+import fs from 'fs';
+import {join} from 'path'
 
-gulp.task('target', () => {
-    return gulp.src([])
-        .pipe(target.pipe()) // add merged target and deps to stream
-        .pipe(gulpIf('config.json', gulpExtend('config.json'))) // by default deep extending
-        .pipe(gulp.dest('.target/targetA'));
-});
+function getDirectories(srcpath) {
+    return fs.readdirSync(srcpath).filter(function(file) {
+        return fs.statSync(join(srcpath, file)).isDirectory();
+    });
+}
 
-gulp.task('build', ['target'], () => {
-    return gulp.src('.target/**/*')
-        .pipe(gulpIf('**/*.{sass,scss}', sass()))
+var buildTarget = function (target) {
+    return lazypipe()
+        .pipe(function () {
+            return target.pipe(); // add merged target and deps to stream
+        })
+        .pipe(function () {
+            return gulpIf('config.json', gulpExtend('config.json'))
+        })
+        .pipe(function () {
+            return gulpIf('**/*.{sass,scss}', sass())
+        })();
+};
+
+gulp.task('build', () => {
+
+    var target = new Target(argv.target || 'common');
+    var task = gulp.src([])
+        .pipe(buildTarget(target))
         .pipe(gulp.dest('dist'));
+
+    if (argv.watch) {
+        gulp.watch(target.sources(), ['build']);
+    }
+    return task;
 });
 
-gulp.task('default', ['build'], function () {
-    gulp.watch(target.sources(), ['target']);
-    gulp.watch('.target/**/*', ['build'])
+gulp.task('export', function () {
+    var targets = getDirectories('targets');
+
+    return targets.map(function (targetName) {
+        return new Target(targetName);
+    }).map(function (target) {
+        return gulp.src([])
+            .pipe(buildTarget(target))
+            .pipe(gulp.dest(join('dist', target.name)))
+    })
+
 });
